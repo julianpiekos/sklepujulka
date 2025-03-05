f52e184684dd40473806c1b35d509f19e65ed876
julianpiekos <julian.piekos2015@gmail.com>
2025-03-05 16:41:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+function przekieruj() {
+    let ocena = document.querySelector('input[name="ocena"]:checked');
+
+    if (!ocena) {
+        alert("Wybierz ocenę przed wysłaniem!");
+        return;
+    }
+
+    let ocenaWartosc = ocena.value;
+
+    let formularzURL = "https://docs.google.com/forms/d/1tF3E6J65EAGlZAVcoWsAlUaJrbG42sytOkmWuesSY-4/viewform";
+
+    let entryID = "entry.1036692998"; 
+
+    let finalURL = `${formularzURL}?usp=pp_url&${entryID}=${ocenaWartosc}`;
+
+    window.location.href = finalURL;
+}
