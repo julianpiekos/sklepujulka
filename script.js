3e2acd9867e0cdb5b56d5fbd871323538c76ccc0
janek0yt <janekytubepl@gmail.com>
2025-03-18 15:19:40 +0000

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js zrobione przez Janka
+document.addEventListener("DOMContentLoaded", () => {
+    const cursor = document.getElementById("cursor");
+
+
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
+    });
+});
+
+function NoweOkno(url) {
+    window.open(url, '_blank', 'width=800,height=600,top=100,left=100');
+}
